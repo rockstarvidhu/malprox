4e8880647a6c698812270dc2bf07cdd07abd6246
rockstarvidhu <vidhumenon007@gmail.com>
2026-07-08 19:22:58 +0530

    data types

---
+++ python_datatypes.py
@@ -0,0 +1,23 @@
+# there are 5 main data types in python
+'''
+1. Numeric
+2. Sequence
+3. Dictionary
+4. Boolean
+5. Set
+'''
+
+a = 10
+print(type(a))
+
+b = 31.04
+print(type(b))
+
+c = 'vidhu0326'
+print(type(c))
+
+d = [1, 2, 3, 4, 5]
+print(type(d))
+
+ed = {'a' :22, 'b' :23}
+print(ed['a'])
