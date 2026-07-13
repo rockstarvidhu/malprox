c2dc35066ac8a9b330b187f23e221aabf5d72bc2
rockstarvidhu <vidhumenon007@gmail.com>
2026-07-13 14:08:06 +0530

    file handling

--- python_exceptions.py
+++ python_exceptions.py
@@ -2,7 +2,7 @@ def divide(a, b):
     return a / b
 
 try:
-    ans = 40 / 0
+    ans = 40 / 0 
 
 except Exception as e:
     print("something went wrong", e)
---
+++ python_filehandling.py
@@ -0,0 +1,10 @@
+file = open('test.txt', mode = 'r')
+
+data = file.readline()
+print(data)
+file.close()
+
+#with open function
+with open('test.txt', mode = 'r') as file:
+    data = file.readline()
+print(data)
